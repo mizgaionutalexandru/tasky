51f223ae1ecad664d4bface636e8d8a6b01c43e9
mizgaionutalexandru <mizgaionutalexandru@gmail.com>
2022-07-12 10:23:58 +0300

    new item style and stretch

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 "use strict";
+
+// New Item Input - make it stretch as it gets more content
+const input = document.querySelector(".item__label__input");
+input.addEventListener("input", () => {
+  input.style.width = "0px";
+  input.style.width = input.scrollWidth + "px";
+});
