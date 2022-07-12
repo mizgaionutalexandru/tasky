bd54508b9cdca9f47e0ae5abaa56d0fbbca5caa1
mizgaionutalexandru <mizgaionutalexandru@gmail.com>
2022-07-12 10:36:08 +0300

    new list style and stretch

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 "use strict";
 
-// New Item Input - make it stretch as it gets more content
-const input = document.querySelector(".item__label__input");
-input.addEventListener("input", () => {
-  input.style.width = "0px";
-  input.style.width = input.scrollWidth + "px";
+// New List Input - make it stretch as it gets more content
+const listInput = document.querySelector(".list__name__input");
+listInput.addEventListener("input", () => {
+  listInput.style.width = "0px";
+  listInput.style.width = listInput.scrollWidth + "px";
 });
+
+// New Item Input - make it stretch as it gets more content
+const itemInput = document.querySelector(".item__label__input");
+itemInput.addEventListener("input", () => {
+  itemInput.style.width = "0px";
+  itemInput.style.width = itemInput.scrollWidth + "px";
+});
+
+// Data Model
+/*
+    const data = {
+        listName: [
+            {
+                completed: true/false,
+                content: text
+            }
+        ]
+    }
+*/
